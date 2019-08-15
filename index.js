4d93f55a8cecb9723069dd2386994bc818fb2c17
Luces Huayhuaca <luces.huayhuaca@gmail.com>
2019-08-15 14:49:27 -0700

    fix then()

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-'use strict'
+'use strict';
 
 class MyPromise {
   constructor(executor) {
-    this._val = null
-    this._thenHandler = null
-    executor(
-      this._resolve.bind(this),
-      this._reject.bind(this)
-    )
+    this._val = null;
+    this._thenHandler = null;
+    this._state = 'pending';
+    executor(this._resolve.bind(this), this._reject.bind(this));
   }
 
   _resolve(val) {
-    this._val = val
+    this._val = val;
     if (this._thenHandler) {
-      this._thenHandler(val)
+      this._thenHandler(val);
     }
+    this._state = 'resolved';
   }
 
-  _reject(err) {
-  }
+  _reject(err) {}
 
   then(thenHandler) {
-    const p = this
+    const p = this;
     return new MyPromise(function thenExec(resolve, reject) {
-      if (p._val) {
-        scheduleThen(p._val)
+      if (p._state == 'resolved') {
+        scheduleThen(p._val);
       } else {
-        p._thenHandler = scheduleThen
+        p._thenHandler = scheduleThen;
       }
       function scheduleThen(val) {
-        setImmediate(
-          () => resolve(thenHandler(val))
-        )
+        setImmediate(() => resolve(thenHandler(val)));
       }
-    })
+    });
   }
 }
 
-module.exports = MyPromise
+module.exports = MyPromise;
