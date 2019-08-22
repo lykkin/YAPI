fc7bc46f2f57f88dfe4638e4f1fae39fa7bbc5f8
Sara Schultz <sschultz@newrelic.com>
2019-08-22 14:55:41 -0700

    add catch

--- index.js
+++ index.js
@@ -16,18 +16,51 @@ class MyPromise {
     this._state = 'resolved';
   }
 
-  _reject(err) {}
+  _reject(val) {
+    this._val = val;
+    if (this._catchHandler) {
+      this._catchHandler(val);
+    }
+    this._state = 'rejected';
+  }
 
   then(thenHandler) {
     const p = this;
     return new MyPromise(function thenExec(resolve, reject) {
       if (p._state == 'resolved') {
         scheduleThen(p._val);
+      } else if (p._state == 'rejected') {
+        setImmediate(() => reject(p._val));
       } else {
         p._thenHandler = scheduleThen;
       }
       function scheduleThen(val) {
-        setImmediate(() => resolve(thenHandler(val)));
+        setImmediate(() => {
+          try {
+          resolve(thenHandler(val));
+        } catch(e) {
+          reject(e);
+        }});
+      }
+    });
+  }
+
+  catch(catchHandler) {
+    const p = this;
+    return new MyPromise(function catchExec(resolve, reject) {
+      if (p._state === 'rejected') {
+        scheduleThen(p._val);
+      } else {
+        p._catchHandler = scheduleThen;
+      }
+      function scheduleThen(val) {
+        setImmediate(() => {
+          try {
+            resolve(catchHandler(val));
+          } catch(e) {
+            reject(e);
+          }
+        });
       }
     });
   }
